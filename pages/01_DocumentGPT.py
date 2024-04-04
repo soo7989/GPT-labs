6b51b93d14b9b8dd949a00bbf550b3d7a001b032
monkey <hs.sanggong@gmail.com>
2024-04-05 03:08:28 +0900

    Streamlit : chat messages

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,3 +1,36 @@
+import time
 import streamlit as st
 
+st.set_page_config(
+    page_title="Document GPT",
+    page_icon="📚",
+)
 st.title("Document GPT")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.write(message)
+    if save:
+        st.session_state["messages"].append({"message": message, "role": role})
+
+
+for message in st.session_state["messages"]:
+    send_message(
+        message["message"],
+        message["role"],
+        save=False,
+    )
+
+message = st.chat_input("Send a message to the AI")
+
+if message:
+    send_message(message, "human")
+    time.sleep(2)
+    send_message(f"You said : {message}", "AI")
+
+    with st.sidebar:
+        st.write(st.session_state)
