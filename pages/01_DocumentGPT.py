cd838de239c52c30238c928031ca11a6574df247
monkey <hs.sanggong@gmail.com>
2024-04-05 04:31:39 +0900

    Streamlit : Uploading documents

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,36 +1,50 @@
 import time
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+from langchain.storage import LocalFileStore
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
 import streamlit as st
 
 st.set_page_config(
     page_title="Document GPT",
     page_icon="📚",
 )
-st.title("Document GPT")
-
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
 
 
-def send_message(message, role, save=True):
-    with st.chat_message(role):
-        st.write(message)
-    if save:
-        st.session_state["messages"].append({"message": message, "role": role})
-
-
-for message in st.session_state["messages"]:
-    send_message(
-        message["message"],
-        message["role"],
-        save=False,
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
     )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
 
-message = st.chat_input("Send a message to the AI")
 
-if message:
-    send_message(message, "human")
-    time.sleep(2)
-    send_message(f"You said : {message}", "AI")
+st.title("Document GPT")
+st.markdown(
+    """
+    안녕, 챗봇을 이용해서 문서의 내용을 정리할 수 있어! 
+    """
+)
 
-    with st.sidebar:
-        st.write(st.session_state)
+file = st.file_uploader(
+    "Upload your txt, pdf, docx",
+    type=["pdf", "docx", "txt"],
+)
+
+if file:
+    retriever = embed_file(file)
+    s = retriever.invoke("김첨지")
+    s
